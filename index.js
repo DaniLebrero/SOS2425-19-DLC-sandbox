262c5a4c887dc1bb0533c25e10317af832ad909e
DaniLebrero <danilebrecar07@gmail.com>
2025-02-21 12:30:13 +0100

    Actualizacion

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
+const cool = require("cool-ascii-faces");
 const app = express();
-const PORT = process.env.port || 16078
+const PORT = process.env.PORT || 16078;
 
-/*function handler(request,response){
-    response.send("Hello from the server  3")
-}
-app.get("/", handler)*/
+app.use("/",express.static("./public"));
 
-app.get("/", (request, response)=>response.send("Hello from the server"))
-app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`);});
+app.get("/hello",(request,response)=>{
+    response.send("Hello from the server!");
+});
 
+app.get("/cool",(request,response)=>{
+    response.send(cool());
+});
 
-var cool  = require('cool-ascii-faces');
-function handler2(request,response){
-    response.send(`${cool()}`)
-}
-app.use("/",express.static("./public"))
-app.get("/cool", (request, response)=>response.send(`${cool()}`))
-app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`);});
-
-console.log(`Finish setup`);
-
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}!`);
+});
